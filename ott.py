fbd70c3870c3fa55707fc843f4dad997644c9297
123tarunanand <anandtarun2@gmail.com>
2018-10-20 11:39:06 +0530

    ott work

--- ott.py
+++ ott.py
@@ -1,2 +1,27 @@
 from RabinMiller import genPrime
 import random
+
+
+def rabinMiller(num):
+    # Returns True if num is a prime number.
+
+    s = num - 1
+    t = 0
+    while s % 2 == 0:
+        # keep halving s while it is even (and use t
+        # to count how many times we halve s)
+        s = s // 2
+        t += 1
+
+    for trials in range(5): # try to falsify num's primality 5 times
+        a = random.randrange(2, num - 1)
+        v = pow(a, s, num)
+        if v != 1: # this test does not apply if v is 1.
+            i = 0
+            while v != (num - 1):
+                if i == t - 1:
+                    return False
+                else:
+                    i = i + 1
+                    v = (v ** 2) % num
+    return True
